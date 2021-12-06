0139c17b7ffbe136ec4129f5d77c681308da2723
Ariel Lutz <ariellutz10@gmail.com>
2021-12-06 14:08:02 -0600

    Hooks y funcion eliminar producto del carrito carrito con alert

--- src/components/Carro/index.js
+++ src/components/Carro/index.js
@@ -1,22 +1,48 @@
 import { useState } from 'react'
 import { Burbuja } from '../Burbuja'
-//import styles from './estilos'
+import styles from './estilos'
+import swal from 'sweetalert'
 import { ContainerCar, ButtonCar, ContainerListArticles, ListArticles, ItemsArticles, ButtonDelete } from './styles'
 
 export const Carro = ({ cantidad, productos }) => {
-
+    
+    const [Productos, setProductos] = useState(productos)
     const [mostrarCarro, setMostrarCarro] = useState(false)
 
+    //Estado para mostrar el carrito de compras
     const handleMostrarCarro = _ => setMostrarCarro(!mostrarCarro)
 
-    let subTotal = productos.reduce((acum, prod) => (prod.cantidad * prod.precio) + acum, 0)
+   //Alerta de confirmar
+   const ConfirmAlert = (id) =>{
+    swal({
+        title:"Eliminar articulo",
+        text: "¿Está seguro de eliminar el artículo del carrito de compra?",
+        buttons: ["No","Si"]
+    }).then(respuesta=>{
+        if(respuesta){
+            eliminarProducto(id)
+        }
+    })
+   }
+
+
+    //Eliminar productos del carrito
+    const eliminarProducto = (id) => {
+        // filtramos la data para no agregar el producto que eliminanos
+        const exist = Productos.find((x) => x.id === id)
+        if (exist) {
+            setProductos(Productos.filter((x) => x.id !== id))
+        }
+    };
+
+    let subTotal = Productos.reduce((acum, prod) => (prod.cantidad * prod.precio) + acum, 0)
     let impto = subTotal * 0.15
     let totalPagar = subTotal + impto
 
     return (
         <ContainerCar>
             {cantidad > 0 && <Burbuja cantidad={cantidad} />}
-            <ButtonCar onClick={handleMostrarCarro}>
+            <ButtonCar style={styles.carro} onClick={handleMostrarCarro}>
                 Carro
             </ButtonCar>
             {
@@ -24,11 +50,11 @@ export const Carro = ({ cantidad, productos }) => {
                     <ContainerListArticles>
                         <ListArticles>
                             {
-                                productos.map(x => {
+                                Productos.map(x => {
                                     return (
                                         <ItemsArticles >
                                             <img height={25} alt={x.nombre} src={x.imagen} />
-                                            <span><ButtonDelete>X</ButtonDelete> {x.nombre}</span>
+                                            <span><ButtonDelete onClick={() => ConfirmAlert(x.id)} >X</ButtonDelete> {x.nombre}</span>
                                             <span>
                                                 ({x.cantidad} x {x.precio.toLocaleString()}) = <strong>{(x.cantidad * x.precio).toLocaleString()}</strong>
                                             </span>
